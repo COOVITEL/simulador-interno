907e9bd9dbf7ebf7ff3ec625d8f3e12c7901facd
ManuRodriguezC <estebanclimb@gmail.com>
2024-08-15 07:50:51 -0500

    update

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { downloadPDF } from "../utils/downloadPdf";
 import Antiguedad from "./form/Antiguedad";
 import EstadoCivil from "./form/EstadoCivil";
@@ -12,12 +12,21 @@ import Ingresos from "./form/Ingresos";
 import { findScore } from "../utils/findScore";
 
 export default function UserForm() {
+    const [control, setControl] = useState(false);
 
-    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const fields = Object.fromEntries(new window.FormData(event.target))
-        const score = findScore(fields)
-        downloadPDF(fields, score)
+    const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setControl(true); // Empieza la visualización de "Descargando..."
+
+        const fields = Object.fromEntries(new window.FormData(event.target));
+        const score = findScore(fields);
+        setTimeout(() => {
+            downloadPDF(fields, score);
+            setControl(false);
+        }, 2000);
+        
+        // Espera la finalización de la descarga
+
     }
 
     return (
@@ -42,10 +51,11 @@ export default function UserForm() {
                 <Antiguedad />
             </div>
             <button
+                type="submit"
                 className="bg-[#1D71B9] w-[400px] rounded-xl text-2xl font-semibold text-white py-2 border-2
                 hover:scale-105 hover:bg-[#2D2D83] duration-300">
-                Calcular
+                {control ? 'Descargando...' : 'Descargar'}
             </button>
         </form>
-    )
+    );
 }
